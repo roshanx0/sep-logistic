5289923c317bd3785cfd6de27e83d1fbd72f9bfe
Roshan <90826638+roshanx0@users.noreply.github.com>
2026-09-18 10:54:00 +0530

    Change minimum input value for AC and fan units

--- App.py
+++ App.py
@@ -8,13 +8,13 @@ st.title("Electrical_Bill AC Price Prediction")
 
 ac_units = st.number_input(
     "Enter AC Units : ",
-    min_value=0.0,
+    min_value=1.0,
     max_value=150.0,
     value=100.0
 )
 fan_units = st.number_input(
     "Enter fan Units : ",
-    min_value=0.0,
+    min_value=1.0,
     max_value=150.0,
     value=100.0
 )
